d3095e7869171c35e9408b4bbf650319e1f38d1a
qzhang <qzhang@treevc.net>
2017-01-08 15:34:00 +0800

    update readme

--- win_kill.php
+++
@@ -1,8 +0,0 @@
-<?php
-function win_kill($pid){ 
-    $wmi=new COM("winmgmts:{impersonationLevel=impersonate}!\\\\.\\root\\cimv2"); 
-    $procs=$wmi->ExecQuery("SELECT * FROM Win32_Process WHERE ProcessId='".$pid."'"); 
-    foreach($procs as $proc) 
-        $proc->Terminate(); 
-}
-win_kill(23076);
